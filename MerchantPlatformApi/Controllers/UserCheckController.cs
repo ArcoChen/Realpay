41403178f63c1c1e3e8e7f3b043949da6b064409
ArcoChen <chenq414@gmail.com>
2017-11-21 08:48:28 +0800

    RealiyPay20171120

---
+++ MerchantPlatformApi/Controllers/UserCheckController.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ReCommon;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace MerchantPlatformApi.Controllers
+{
+    public class UserCheckController : ApiController
+    {
+
+    }
+}
---
+++ OperationPlatformApi/Controllers/OpUserCheckController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OperationPlatformModel;
+using ReCommon;
+using System.Web;
+using Newtonsoft.Json;
+using System.Text;
+using System.Configuration;
+using CacheManager;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RedisModel;
+
+namespace OperationPlatformApi.Controllers
+{
+    public class OpUserCheckController : ApiController
+    {
+        /// <summary>
+        /// 获取手机验证码
+        /// </summary>
+        /// <param name="UserMobile">手机号</param>
+        /// <returns>验证码</returns>
+        [HttpPost]
+        public HttpResponseMessage GetAuthCode(BaseModel model)
+        {
+            string Result = string.Empty;
+
+            try
+            {
+                //string username = "DataSnapDebugTools";
+                string username = HttpContext.Current.Request.RequestContext.RouteData.Values["controller"].ToString();
+                string password = ConfigurationManager.AppSettings[username];
+                string Url = ApiHelper.GetURL(username);
+
+
+                //请求中包含的固定参数
+                model.SOURCE = ParametersFilter.FilterSqlHtml(model.SOURCE, 15);
+                model.CREDENTIALS = ParametersFilter.FilterSqlHtml(model.CREDENTIALS, 10);
+                model.ADDRESS = HttpHelper.IPAddress();
+                model.TERMINAL = ParametersFilter.FilterSqlHtml(model.TERMINAL, 1);
+                model.INDEX = ParametersFilter.FilterSqlHtml(model.INDEX, 14);
+                model.METHOD = ParametersFilter.FilterSqlHtml(model.METHOD, 15);
+
+                //去除提交的数据中的不安全字符
+                model.UserMobile = ParametersFilter.FilterSqlHtml(model.UserMobile, 11);
+
+                var JSetting = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+
+                string Str = JsonConvert.SerializeObject(model, JSetting);
+
+                Result = ApiHelper.HttpRequest(username, password, Url, Str);
+
+                JObject json = (JObject)JsonConvert.DeserializeObject(Result);
+
+                if (json["DATA"][0]["result"].ToString() == "true")
+                {
+                    string AuthCode =  ApiHelper.HttpRequest(ApiHelper.GetAuthCodeURL(), model);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                LogHelper.Error(ex.ToString());
+            }
+            HttpResponseMessage Respond = new HttpResponseMessage { Content = new StringContent(Result, Encoding.GetEncoding("UTF-8"), "application/json") };
+            return Respond;
+        }
+
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public HttpResponseMessage UserLogin(UserInfoModel model)
+        {
+            string Result = string.Empty;
+
+            //string username = "DataSnapDebugTools";
+            string username = HttpContext.Current.Request.RequestContext.RouteData.Values["controller"].ToString();
+            string password = ConfigurationManager.AppSettings[username];
+            string Url = ApiHelper.GetURL(username);
+
+            try
+            {
+                //请求中包含的固定参数
+                model.SOURCE = ParametersFilter.FilterSqlHtml(model.SOURCE, 15);
+                model.CREDENTIALS = ParametersFilter.FilterSqlHtml(model.CREDENTIALS, 10);
+                model.ADDRESS = HttpHelper.IPAddress();
+                model.TERMINAL = ParametersFilter.FilterSqlHtml(model.TERMINAL, 1);
+                model.INDEX = ParametersFilter.FilterSqlHtml(model.INDEX, 14);
+                model.METHOD = ParametersFilter.FilterSqlHtml(model.METHOD, 15);
+
+                //去除参数中的特殊字符
+                model.UserAccount = ParametersFilter.FilterSqlHtml(model.UserAccount, 30);
+                model.UserPasswd = ParametersFilter.FilterSqlHtml(model.UserPasswd, 64);
+
+                //序列化
+                var JSetting = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+
+                string Str = JsonConvert.SerializeObject(model, JSetting);
+
+                //http请求
+                Result = ApiHelper.HttpRequest(username, password, Url, Str);
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error(ex.Message);
+            }
+
+            //返回请求结果
+            HttpResponseMessage Respend = new HttpResponseMessage { Content = new StringContent(Result, Encoding.GetEncoding("UTF-8"), "application/json") };
+            return Respend;
+        }
+
+        /// <summary>
+        /// 手机号登录
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public HttpResponseMessage MobileLogin(BaseModel model)
+        {
+            string Result = string.Empty;
+
+            //string username = "DataSnapDebugTools";
+            string username = HttpContext.Current.Request.RequestContext.RouteData.Values["controller"].ToString();
+            string password = ConfigurationManager.AppSettings[username];
+            string Url = ApiHelper.GetURL(username);
+
+            try
+            {
+                //请求中包含的固定参数
+                model.SOURCE = ParametersFilter.FilterSqlHtml(model.SOURCE, 15);
+                model.CREDENTIALS = ParametersFilter.FilterSqlHtml(model.CREDENTIALS, 10);
+                model.ADDRESS = HttpHelper.IPAddress();
+                model.TERMINAL = ParametersFilter.FilterSqlHtml(model.TERMINAL, 1);
+                model.INDEX = ParametersFilter.FilterSqlHtml(model.INDEX, 14);
+                model.METHOD = ParametersFilter.FilterSqlHtml(model.METHOD, 15);
+
+                //去除参数中的特殊字符
+                model.UserMobile = ParametersFilter.FilterSqlHtml(model.UserMobile, 11);
+                model.Verification = ParametersFilter.FilterSqlHtml(model.Verification, 6);
+
+                //实例化Redis请求参数
+                BaseModel redis = new BaseModel();
+
+                redis.RedisIP = "127.0.0.1";
+                redis.RedisPort = "6379";
+                redis.RedisPassword = "yg50";
+                redis.RedisKey = "AuthCode_" + model.UserMobile;
+                redis.RedisValue = model.Verification;
+                redis.LifeCycle = "60";
+                redis.RedisFunction = "StringGet";
+
+                //获取Redis中的验证码
+                string GetRedisAuthCode = ApiHelper.HttpRequest(ApiHelper.GetRedisURL(redis.RedisFunction), redis);
+
+                if (GetRedisAuthCode == "null")
+                {
+                    Result = "{\"DATA\":[{\"result\":\"验证码已过时\"}]}";
+                }
+                else if (GetRedisAuthCode == model.Verification)
+                {
+                    var JSetting = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+
+                    string Str = JsonConvert.SerializeObject(model, JSetting);
+
+                    Result = ApiHelper.HttpRequest(username, password, Url, Str);
+                }
+                else
+                {
+                    Result = "{\"DATA\":[{\"result\":\"验证码错误\"}]}";
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error(ex.Message);
+            }
+
+            //返回请求结果
+            HttpResponseMessage Respend = new HttpResponseMessage { Content = new StringContent(Result, Encoding.GetEncoding("UTF-8"), "application/json") };
+            return Respend;
+        }
+    }
+}
